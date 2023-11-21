7f3d377c19a5e7728df514cea0d0ba936092ed36
gisimclean@gmail.com <138517546+GiselleDM@users.noreply.github.com>
2023-11-20 21:34:52 -0500

    rest of files

--- world.js
+++ world.js
@@ -1,27 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Listen for clicks on the 'Lookup' button
-    document.getElementById('lookup').addEventListener('click', function() {
-        // Get the input value (country name) from the user
-        var country = document.getElementById('country').value;
-        
-        // Create an XMLHttpRequest object
-        var xhr = new XMLHttpRequest();
-        
-        // Define the AJAX request
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    // Update the 'result' div with the fetched data
-                    document.getElementById('result').innerHTML = xhr.responseText;
-                } else {
-                    // Handle error states here
-                    console.error('Error fetching data');
-                }
-            }
-        };
+document.getElementById('lookup').addEventListener('click', function() {
+    const country = document.getElementById('country').value;
+    const url = `world.php?country=${country}`;
 
-        // Open a GET request to world.php with the country name as a parameter
-        xhr.open('GET', 'world.php?country=' + country, true);
-        xhr.send(); // Send the request
-    });
+    fetchCountryData(url);
 });
+
+document.getElementById('lookupCities').addEventListener('click', function() {
+    const country = document.getElementById('country').value;
+    const url = `world.php?country=${country}&lookup=cities`;
+
+    fetchCityData(url);
+});
+
+function fetchCountryData(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('result').innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function fetchCityData(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('result').innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
+}
--- world.php
+++ world.php
@@ -1,32 +1,49 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'Password123';
 $dbname = 'world';
 
-// Check if the 'country' parameter is provided in the URL
-if (isset($_GET['country'])) {
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+if(isset($_GET['country'])) {
     $country = $_GET['country'];
+    if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+        $stmt = $conn->prepare("SELECT Name, District, Population FROM city WHERE CountryCode = (SELECT Code FROM country WHERE Name = :country)");
+        $stmt->bindParam(':country', $country);
+        $stmt->execute();
 
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name = :country");
-    $stmt->bindParam(':country', $country, PDO::PARAM_STR);
-    $stmt->execute();
-
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        // Display country details
-        echo "<h1>{$result['name']}</h1>";
-        echo "<p>Population: {$result['population']}</p>";
-        echo "<p>Head of State: {$result['head_of_state']}</p>";
-        // Add more information as needed
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($results && count($results) > 0) {
+            echo "<table border='1'>";
+            echo "<tr><th>Name</th><th>District</th><th>Population</th></tr>";
+            foreach ($results as $row) {
+                echo "<tr>";
+                echo "<td>" . $row['Name'] . "</td>";
+                echo "<td>" . $row['District'] . "</td>";
+                echo "<td>" . $row['Population'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No cities found for the specified country.";
+        }
     } else {
-        // Country not found
-        echo "<p>Country not found</p>";
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        $stmt->bindValue(':country', '%' . $country . '%');
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($results && count($results) > 0) {
+            echo "<ul>";
+            foreach ($results as $row) {
+                echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "No countries found matching the search criteria.";
+        }
     }
 } else {
-    // Handle if 'country' parameter is not provided
-    echo "<p>Please provide a 'country' parameter in the URL.</p>";
+    echo "Please provide a country name.";
 }
 ?>
